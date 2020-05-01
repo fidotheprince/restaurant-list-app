30a9c9496d3bcc44a768291ad2b55a795de244ea
Alvaro Castro Cid <58068322+fidotheprince@users.noreply.github.com>
2020-04-30 18:35:42 -0700

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using TestData.Web.App;
+
+namespace TestData.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //register Container
+            ContainerConfig.RegisterContainer();
+        }
+    }
+}
